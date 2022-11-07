627c3a6822c66290052b26c4a0fa51cd6d393437
weggeeee <94621041+weggeeee@users.noreply.github.com>
2022-11-07 01:48:39 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          This page is owned by nweckar.
         </p>
 
         <div className={styles.grid}>
