bb6546b18bba8f207fe70febdf22432c5f49200c
weggeeee <94621041+weggeeee@users.noreply.github.com>
2022-11-10 19:04:21 -0500

    Update index.js
    Added link to chess

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+
 
 export default function Home() {
   return (
@@ -13,7 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome, play chess on <Link href="/chess">this page!</Link>
         </h1>
 
         <p className={styles.description}>
